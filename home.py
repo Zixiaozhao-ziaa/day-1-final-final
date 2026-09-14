3549f00b0d36a23cac68a3c0ba2680dd39fa64ad
Zixiaozhao-ziaa <zixiaozhao@student.unimelb.edu.au>
2026-09-14 05:52:55 +0000

    111

--- home.py
+++ home.py
@@ -4,4 +4,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-##ask 
+st.title(LAWS90286)
+
+##store import variable in a session_state
+if "name" not in st.session_state:
+    st.session_state.button_value = False
+
+##ask the user enter their name
+st.header("Part 1 - Get Name")
+name = st.text_input("What is your name?")
+
+##if 
+if name =="Jack":
+    st.write("welcome")
